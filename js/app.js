8d04536bd5c588c395b9aefe2bc46c2c0212fa74
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-03-30 01:17:05 -0700

    updated index, css, and js files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,58 @@
-alert('connected to page');
+const Questions = [
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+{
+    question: "Who is the strongest avenger", 
+        'choice1': 'Captin America',
+        'choice2': 'Hulk',
+        'choice3': 'Thor',
+    correctAnswer: ''
+
+},
+
+];
+
+
