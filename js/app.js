8c909d201d8070e9e37aae23fc84bccd150c2740
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-03-30 23:52:50 -0700

    added sections to index file, gather imgs and assets

--- js/app.js
+++ js/app.js
@@ -1,58 +1,79 @@
 const Questions = [
 {
     question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
+    questionChoices: ['Captain America','Hulk','Thor'],
+    correctAnswer: 'Thor',
 
 },
 
 {
-    question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
+    question: "Who is the most skilled fighter of the Avengers", 
+    questionChoices:['Black Panther','Black Widow','Captain America'],
+    correctAnswer: 'Black Widow',
 
 },
 
 {
-    question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
+    question: "Who is Ronin", 
+    questionChoices:[ 'Black Widow','Hawkeye','Task Master'],
+    correctAnswer: 'Hawkeye'
 
 },
 
 {
-    question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
+    question: "Who is the tactical leader of the avengers", 
+    questionChoices: [ 'Captin America','Tony Stark','Nick Fury'],
+    correctAnswer: 'Captain America'
 
 },
 
 {
-    question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
-
-},
-
-{
-    question: "Who is the strongest avenger", 
-        'choice1': 'Captin America',
-        'choice2': 'Hulk',
-        'choice3': 'Thor',
-    correctAnswer: ''
+    question: "Who is the smartest avenger", 
+    questionChoices: ['Shuri','Bruce Banner','Tony Stark'],
+    correctAnswer: 'Tony Stark'
 
 },
 
 ];
+
+let score = 0;
+let questionNumber = 0;
+
+//this functions loads the questions into the question page
+function renderQuestions() {
+
+}
+
+//this function handles when the user clicks the submit
+function checkAns() {
+    
+}
+
+//listen to the click on next btn if answer is correct 
+    // user is taken to next question 
+function nextQuestion(){
+    
+}
+
+// func will see if ans is correct and then update score
+function updateScore(){
+    
+}
+
+
+//this will reset the score of the app and takes user to the beginning
+function resetQuiz(){
+    
+}
+
+function main(){
+    
+    renderQuestions();
+    checkAns();
+    nextQuestion();
+    resetQuiz();
+}
+
+$(main());
 
 
