9a83f6992b20f80a77e46a5ae3f5dfa3de080215
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-03-31 09:47:59 -0700

    updated buttons to trigger next step in quiz

--- js/app.js
+++ js/app.js
@@ -47,30 +47,67 @@ function init(){
         $('.correct').hide();
         $('.wrong').hide();
         $('#score').hide();
+        renderQuestions()
         console.log('started quiz');
     })
+
+
+    $('#quiz').on('submit', function(event){
+        event.preventDefault();
+
+        $('#start').hide();
+        $("#questions").hide();
+        $('.correct').show();
+        $('.wrong').hide();
+        $('#score').hide();
+        checkAns()
+        console.log('submitted ans to question');
+    })
+
+    $('.ans-feedback').on('click', function(){
+        $('#start').hide();
+        $("#questions").hide();
+        $('.correct').hide();
+        $('.wrong').hide();
+        $('#score').show();
+        renderQuestions()
+        nextQuestion()
+        console.log('submitted time to get total');
+    })
+
+    $('#reset').on('click', function(){
+        $('#start').show();
+        $("#questions").hide();
+        $('.correct').hide();
+        $('.wrong').hide();
+        $('#score').hide();
+        resetQuiz()
+        console.log('go to beginning');
+    })
+
 }
 
 //this functions loads the questions into the question page
 function renderQuestions() {
-    
+    console.log('inside render question');
 
 }
 
 //this function handles when the user clicks the submit
 function checkAns() {
-    
+    console.log('inside check answer');
+    nextQuestion()
 }
 
 //listen to the click on next btn if answer is correct 
     // user is taken to next question 
 function nextQuestion(){
-    
+    console.log('inside next question');
 }
 
 // func will see if ans is correct and then update score
 function updateScore(){
-    
+    console.log('inside update score');
 }
 
 
@@ -79,14 +116,6 @@ function resetQuiz(){
     
 }
 
-function main(){
-    init();
-    renderQuestions();
-    checkAns();
-    nextQuestion();
-    resetQuiz();
-}
-
-$(main());
+$(init());
 
 
